55fe3a7d9a3eed100f05d3cb9ed43c62073e1cab
Manda Tran <mtt040@ucsd.edu>
2020-09-11 00:06:22 -0700

    start template for project descriptions

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,3 @@
-/*
-import smoothscroll from "smoothscroll-polyfill";
-
-// kick off the polyfill!
-smoothscroll.polyfill();
-*/
-
 /************************************************************
  * Underline Menu Bar on Scroll
  * src: https://codepen.io/jasperreyes/pen/doqgdP
@@ -16,28 +9,3 @@ $(window).scroll(function() {
       $('.navbar').removeClass('underlineNav');
   }
 });
-
-/************************************************************
- * Smooth Scroll Safari fix
- * src: https://stackoverflow.com/questions/51229742/javascript-window-scroll-behavior-smooth-not-working-in-safari
-************************************************************/
-/*
-function SmoothVerticalScrolling(e, time, where) {
-  var eTop = e.getBoundingClientRect().top;
-  var eAmt = eTop / 100;
-  var curTime = 0;
-  while (curTime <= time) {
-      window.setTimeout(SVS_B, curTime, eAmt, where);
-      curTime += time / 100;
-  }
-}
-
-function SVS_B(eAmt, where) {
-  if(where == "center" || where == "")
-      window.scrollBy(0, eAmt / 2);
-  if (where == "top")
-      window.scrollBy(0, eAmt);
-}
-
-SmoothVerticalScrolling('#projects', 275, "center");
-*/
