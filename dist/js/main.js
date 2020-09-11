cf426d826886d2791d9b75cbcad64a9eecd14846
Manda Tran <mtt040@ucsd.edu>
2020-09-10 20:43:06 -0700

    add fixes to top nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,18 @@
+/************************************************************
+ * Underline Menu Bar on Scroll
+ * src: https://codepen.io/jasperreyes/pen/doqgdP
+************************************************************/
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 10) {
+      $('.navbar').addClass('underlineNav');
+  } else {
+      $('.navbar').removeClass('underlineNav');
+  }
+});
+
+
+
+/*
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
@@ -26,35 +41,4 @@ function toggleMenu() {
         navItems.forEach(item => item.classList.remove('open'));
     }
 }
-
-
-/*
- * Handle pop-up quick view for Projects
- */
-
- // Get the modal
-var modal = document.getElementsByClassName("projects__quick-view");
-
-// Get the button that opens the modal
-var btn = document.getElementsByClassName("projects__item");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    alert("Hello");
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+*/
