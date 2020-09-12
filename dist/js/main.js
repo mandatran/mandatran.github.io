73bbce34440e0b8c667fb09635937ac8af98a00b
Manda Tran <mtt040@ucsd.edu>
2020-09-12 00:03:57 -0700

    add new portfolio archives details page

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,17 @@ $(window).scroll(function() {
       $('.navbar').removeClass('underlineNav');
   }
 });
+
+
+/************************************************************
+ * change preview images in project page on click
+ * src: https://codepen.io/jasperreyes/pen/doqgdP
+************************************************************/
+var counter = 1;
+function changePreview() {
+
+  var webPreviews = ["../archive/v4/index.html", "../archive/v3/index.html", "../archive/v2/index.html", "../archive/v1/index.html"]
+
+  document.getElementById("web-preview").src = webPreviews[counter%webPreviews.length];
+  counter++;
+}
